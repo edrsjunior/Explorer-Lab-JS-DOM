c755f247aff33229c8247a7fd6d5e09466278f9e
Edilson Junior <edilsonrsjunior@gmail.com>
2022-10-19 20:46:37 -0300

    create masks from card with regex and functions

--- src/main.js
+++ src/main.js
@@ -45,3 +45,39 @@ const  expirationDatePattern = {
 }
 
 const expirationDateMasked = IMask(ccExpirationDate,expirationDatePattern)
+
+// CARD NUMBER MASK
+const ccNumber = document.querySelector("#card-number")
+const ccNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|2[3-7]\d{0,2}\d{0,12})/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+
+  
+
+
+  dispatch: function (appended, dynamicMasked) {// CRIAR UMA FUNCAO dispatch que funciona semelherante a recursao ou seja ela chama ela mesma
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "") //Pega cada caractere e adiciona na dynamicMasked e caso nao for um digito substitui por nada
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) { //Procura uma mascara valida no array de mask ccNumberPattern e caso encontre atraves do match retorna
+      return number.match(item.regex)
+    })
+
+    console.log(foundMask)
+    return foundMask
+  },
+}
+
+const ccNumberMasked = IMask(ccNumber,ccNumberPattern)
